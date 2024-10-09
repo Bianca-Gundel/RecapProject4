f8f25f4a54155e879307cf762b35d712e9d0f9e8
Bianca <gundel.biancaw@web.de>
2024-10-09 15:17:50 +0200

    Add ColorAddFunction and ColorForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
 import "./App.css";
+import ColorForm from "./Components/ColorForm/ColorForm.jsx";
+import { uid } from "uid";
+import { useState } from "react";
 
 function App() {
+  const [colors, setColors] = useState(initialColors);
+
+  function handleSubmit(newColor) {
+    setColors([{ id: uid(), ...newColor }, ...colors]);
+  }
+
   return (
     <>
       <h1>Theme Creator</h1>
+      <ColorForm onSubmit={handleSubmit} />
 
-      {initialColors.map((color) => {
+      {colors.map((color) => {
         return <Color key={color.id} color={color} />;
       })}
     </>
