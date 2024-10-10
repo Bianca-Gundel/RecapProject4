a147003e20cf808f67a9ac150444cc2045ed3f9d
Bianca <gundel.biancaw@web.de>
2024-10-10 10:38:51 +0200

    Add localStorage to colors

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import Color from "./Components/Color/Color";
 import "./App.css";
 import ColorForm from "./Components/ColorForm/ColorForm.jsx";
 import { uid } from "uid";
-import { useState } from "react";
+import useLocalStorageState from "use-local-storage-state";
 
 function App() {
-  const [colors, setColors] = useState(initialColors);
+  const [colors, setColors] = useLocalStorageState("colors", {
+    defaultValue: initialColors,
+  });
 
   function handleSubmit(newColor) {
     setColors([{ id: uid(), ...newColor }, ...colors]);
